8bbf6f499a8da11cbb5b01173907fac715a72de9
aeez <makramfais@gmail.com>
2022-11-06 11:12:02 +0700

    cantikin desain error login

--- admin/auth_login.php
+++ admin/auth_login.php
@@ -13,20 +13,20 @@ if (isset($_POST['login'])) {
     $password = $_POST['password'];
 
     $petugasInfo = mysqli_query($conn, "SELECT * FROM petugas WHERE username = '$username'");
-    $siswaInfo = mysqli_query($conn, "SELECT * FROM siswa WHERE username = '$username'");
+    // $siswaInfo = mysqli_query($conn, "SELECT * FROM siswa WHERE username = '$username'");
 
     if (mysqli_num_rows($petugasInfo) === 0) {
         $error4 = true;
-    } else if (mysqli_num_rows($siswaInfo) === 0) {
-        $error4 = true;
-    }
+    } //else if (mysqli_num_rows($siswaInfo) === 0) {
+        //$error4 = true;
+    //}
 
     if (mysqli_num_rows($petugasInfo) === 1) {
 
         $row = mysqli_fetch_assoc($petugasInfo);
 
         if (password_verify($password, $row['password'])) {
-            // sessin set
+            // session set
             $_SESSION['login'] = true;
             $_SESSION['nama'] = $row['nama_petugas'];
             $_SESSION['level'] = $row['level'];
@@ -35,21 +35,21 @@ if (isset($_POST['login'])) {
         }
 
         $error = true;
-    } else if (mysqli_num_rows($siswaInfo) === 1) {
+    } //else if (mysqli_num_rows($siswaInfo) === 1) {
 
-        $row = mysqli_fetch_assoc($siswaInfo);
+    //     $row = mysqli_fetch_assoc($siswaInfo);
 
-        if (password_verify($password, $row['password'])) {
-            // sessin set
-            $_SESSION['login'] = true;
-            $_SESSION['nama'] = $row['nama_siswa'];
-            $_SESSION['level'] = $row['level'];
-            header('location: home.php');
-            exit;
-        }
+    //     if (password_verify($password, $row['password'])) {
+    //         // sessin set
+    //         $_SESSION['login'] = true;
+    //         $_SESSION['nama'] = $row['nama_siswa'];
+    //         $_SESSION['level'] = $row['level'];
+    //         header('location: home.php');
+    //         exit;
+    //     }
 
-        $error = true;
-    }
+    //     $error = true;
+    // }
 }
 ?>
 
@@ -75,51 +75,52 @@ if (isset($_POST['login'])) {
 
     <!-- form -->
     <section class="container-fluid form-page">
-        <div
-        class="row justify-content-center align-items-center"
-        style="height: 100vh">
+        <div class="row justify-content-center align-items-center" style="height: 100vh">
             <div class="col-10 col-lg-4 p-3 form-wrapper">
-            <div class="form-logo text-center my-5">
-                <h1 class="fw-bold fst-italic"><i class="bi bi-x-lg"></i> DIRENT</h1>
+                <div class="form-logo text-center my-5">
+                    <h1 class="fw-bold fst-italic"><i class="bi bi-x-lg"></i> DIRENT</h1>
+                </div>
+                
                 <?php if (isset($error)) { ?>
                     <div class="alert alert-danger alert-dismissible" role="alert">
                         Gagal, mohon periksa lagi username dan password Anda!
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                     </div>
                 <?php } else if (isset($error4)) { ?>
                     <div class="alert alert-danger alert-dismissible" role="alert">
                         Gagal, Anda belum terdaftar!
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                     </div>
                 <?php }  ?>
-            </div>
-            <div class="form-main">
-                <form action="" method="POST">
-                <div class="mb-3">
-                <input
-                    type="text"
-                    class="form-control"
-                    name="username"
-                    id="username"
-                    placeholder="username"
-                    required
-                />
+                <div class="form-main">
+                    <form action="" method="POST">
+                        <div class="mb-3">
+                            <input
+                                type="text"
+                                class="form-control"
+                                name="username"
+                                id="username"
+                                placeholder="Username"
+                                required
+                            />
+                        </div>
+                        <div class="mb-4">
+                            <input
+                                type="password"
+                                class="form-control"
+                                name="password"
+                                id="password"
+                                placeholder="Password"
+                                required
+                            />
+                        </div>
+                        <div class="form-button mb-5">
+                            <button type="submit" name="login">Log In</button>
+                        </div>
+                    </form>
                 </div>
-                <div class="mb-4">
-                <input
-                    type="password"
-                    class="form-control"
-                    name="password"
-                    id="password"
-                    placeholder="password"
-                    required
-                />
-                </div>
-                <div class="form-button mb-5">
-                <button type="submit" name="login">Sign In</button>
-                </div>
-                </form>
             </div>
-            </div>
-      </div>
+        </div>
     </section>
     <!-- form -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
